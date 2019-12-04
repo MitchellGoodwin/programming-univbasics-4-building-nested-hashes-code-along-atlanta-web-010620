b4659aea0d44686182f63e85c94344685e6c08a0
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-04 21:06:26 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,10 +15,11 @@ def third_challenge
    :capulet => {
       :patriarch => {
         #your key/value pairs here
+        
         },
       :matriarch => {
         #your key/value pairs here
-        
+        name: "Lady Capulet", age: "51"
         },
       :heroine => {
         #your key/value pairs here
